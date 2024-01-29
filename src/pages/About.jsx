813dfc8918cdad89792885f0eda982b6b1caeb3a
abhishek <abhishekyadav49534@gmail.com>
2024-01-30 01:04:26 +0530

    creates pages and routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>Hello i m About</h1>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/SignOut.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SignOut() {
+  return (
+    <div>
+      <h2>Signout</h2>
+    </div>
+  );
+}
+
+export default SignOut;
