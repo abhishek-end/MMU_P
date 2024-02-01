8412ad454ab5a4d38f2b0ffb2535d0c2750c81d3
abhishek <abhishekyadav49534@gmail.com>
2024-02-02 02:22:38 +0530

    create function to handle errorrs

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,7 +1,8 @@
 import User from  "../models/user.model.js";
 import bcrypt from "bcrypt"
+import handleError from '../utils/error.js'
 
-export const  signup =async (req,res) => {
+export const  signup =async (req,res , next) => {
 const {username, email , password} = req.body;
 const hashedPasswrod =  bcrypt.hashSync(password,10) 
 //hashsync is used here which make hashedPassword await without using await 
@@ -10,7 +11,7 @@ try {
     await newUser.save();
 res.status(201).json('User Created Successfully')
 } catch (error) {
-   res.status(500).json(error.message)
+   next(handleError(500, 'Internal Server Error'))
 }
 }
 export default signup
